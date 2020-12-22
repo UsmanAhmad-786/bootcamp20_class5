25ae51ccaf1dcd16d3f04ad98a4b96dbbdebc512
Usman Ahmad <usman.ahmad6786@gmail.com>
2020-12-22 17:53:19 +0500

    add default case

--- src/CountReducer.js
+++ src/CountReducer.js
@@ -3,7 +3,9 @@
 const CountReducer =(state , action)=>{
     switch(action){
         case 'Increment':
-        return state+1; 
+            return state+1; 
+        default:
+            throw new Error()
     }
     //similarly we can decrement
 
